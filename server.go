763aff994512485ce93cd4cac2b328b4c8a2409e
Alex Oberhofer <alexmoberhofer@gmail.com>
2020-07-08 12:31:58 -0400

    you can now pass the port in

--- server.go
+++ server.go
@@ -1,46 +1,76 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// A simple web server
+//
+// License: GNU GPLv3
+// (C) 2020 Alex Oberhofer
+///////////////////////////////////////////////////////////////////////////////
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-var home_counter int = 0
+var verbose = 1
 
 /**
 * Handler function for routing to the home page
-*/
+ */
 func handleHome(w http.ResponseWriter, r *http.Request) {
-    home_counter++
-    http.ServeFile(w, r, "index.html")
-    fmt.Printf("Home Handler called. Count: %d\n", home_counter)
+	http.ServeFile(w, r, "index.html")
+	fmt.Printf("Home handler called...\n")
 }
 
+func logStdOut(logMessage string) {
+	if verbose > 0 {
+		fmt.Printf(logMessage)
+		fmt.Printf("\n")
+	}
+}
+
+/**
+* Start server and assign fileservers and handlers
+ */
 func startServer() {
-    fmt.Printf("Attempting to start web server...\n")
-    
-    //Initialize File Server
-    http.Handle("/", http.FileServer(http.Dir("./")))
+	logStdOut("Attempting to start web server...")
 
-    //add a home handler
-    http.HandleFunc("./", handleHome)
+	//Initialize File Server
+	http.Handle("/", http.FileServer(http.Dir("./")))
 
-    //add ping handler
-    http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Pong!")
-    })
+	//add a home handler
+	http.HandleFunc("./", handleHome)
 
-    fmt.Printf("Server initialized successfully!\n")
+	//add ping handler
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Pong!")
+	})
 
+	logStdOut("Server initialized successfully!")
+
+}
+
+/*
+* Print usage message to console
+ */
+func usage() {
+	fmt.Printf("go run server.go \n")
+	fmt.Printf("Flags: \n")
+	fmt.Printf("p <port> -v <log level>\n")
 }
 
 func main() {
 
-    startServer()
+	port := flag.String("p", "8080", "web server port")
+	//TODO: pull verbose flag here
+	flag.Parse()
 
-    fmt.Printf("Server started on port 8080. Press Ctrl + C to exit.\n")
+	startServer()
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("Server started on port %s. Press Ctrl + C to exit.\n", *port)
+
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 
 }
